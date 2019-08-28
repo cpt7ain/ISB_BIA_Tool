e4ffcb568fe0ece258300fc110c6260bb7827e94
cpt7ain <tim180194@gmail.com>
2019-08-28 17:46:35 +0200

    nn

---
+++ Matching CMDB SBA Kennungen.sql
@@ -0,0 +1,17 @@
+﻿--Drop Table Test_App
+--SELECT * INTO [dbo].[Test_App] 
+--FROM [dbo].[ISB_BIA_Applikationen]
+--go
+--Alter table Test_App 
+--add Kennung varchar(40)
+--update Test_App set Kennung = "a" where 
+
+--UPDATE Test_App t1
+--INNER JOIN Test_Relation as t2 ON t1.App_ID = t2.App_ID
+--SET t1.Kennung = t2.Kennung
+
+--Befüllen der Applikationstabelle mit den Kennungen der CMDB über die in Excel erfassten Relationen (ISB...original.xlsx)
+UPDATE t1
+SET t1.Kennung = t2.Kennung
+from Test_App as t1
+INNER JOIN Test_Relation as t2 ON t1.Applikation_ID = t2.App_ID
